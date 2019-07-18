91c25c077e6f165e4d9c5fb0da3a8ff8079f8695
Mylab <labs@onmycrowd.com>
2019-07-17 21:14:03 -0700

    Finishing with implementation of the notify method (send message to telegram group).

--- src/main/java/com/busqandote/scraper/AppointmentScraper.java
+++ src/main/java/com/busqandote/scraper/AppointmentScraper.java
@@ -1,19 +1,17 @@
 package com.busqandote.scraper;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URISyntaxException;
 
 public class AppointmentScraper {
     private HttpClient httpClient;
@@ -27,18 +25,17 @@ public class AppointmentScraper {
     public boolean check() {
         String url = "http://visas.migracion.gob.pa/SIVA/verif_citas";
 
-        Document document = null;
         try {
-            document = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).get();
+            Document document = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).get();
+
+            Elements images = document.select("img");
+            Elements links = document.select("a");
+
+            if(images.size() > 2) {
+                return links.size() > 1;
+            }
         } catch (IOException ex) {
             ex.printStackTrace();
-        }
-
-        Elements images = document.select("img");
-        Elements links = document.select("a");
-
-        if(images.size() > 2) {
-            return links.size() > 1;
         }
 
         return false;
@@ -46,21 +43,26 @@ public class AppointmentScraper {
 
     public boolean send(String message, String user) {
         String url = "https://wlwba0hr6h.execute-api.us-east-1.amazonaws.com/Prod/message";
-        HttpPost httpPost = new HttpPost(url);
 
         try {
-            List<NameValuePair> params = new ArrayList<>();
-            params.add(new BasicNameValuePair("message", message));
-            params.add(new BasicNameValuePair("user", user));
-            httpPost.setEntity(new UrlEncodedFormEntity(params));
+            URIBuilder builder = new URIBuilder(url);
+            builder.setParameter("message", message).setParameter("user", user);
+
+            HttpPost httpPost = new HttpPost(builder.build());
             httpPost.setHeader("Content-type", "application/json");
 
             HttpResponse response = httpClient.execute(httpPost);
             StatusLine statusLine = response.getStatusLine();
 
-            if(statusLine.getStatusCode() != 200)
+            if(statusLine.getStatusCode() != 200) {
+                String json = EntityUtils.toString(response.getEntity(), "UTF-8");
+                System.out.println("The one: " + json);
                 return false;
+            }
         } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        } catch (URISyntaxException e) {
             e.printStackTrace();
             return false;
         }
