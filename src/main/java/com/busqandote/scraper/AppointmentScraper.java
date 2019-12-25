2d5d8564b90664b83b8e434e82ba794fe8bb71a1
Mylab <labs@onmycrowd.com>
2019-12-24 19:34:18 -0700

    Implementing a socket factory to avoid ssl certificate error while using jsoup.

--- src/main/java/com/busqandote/scraper/AppointmentScraper.java
+++ src/main/java/com/busqandote/scraper/AppointmentScraper.java
@@ -10,8 +10,15 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 
 public class AppointmentScraper {
     private HttpClient httpClient;
@@ -26,7 +33,7 @@ public class AppointmentScraper {
         String url = System.getenv("SCRAP_URL");
 
         try {
-            Document document = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).get();
+            Document document = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).sslSocketFactory(socketFactory()).get();
 
             Elements images = document.select("img");
             Elements links = document.select("a");
@@ -59,9 +66,7 @@ public class AppointmentScraper {
                 System.out.println("The one: " + json);
                 return false;
             }
-        } //catch (ConnectionPoolTimeoutException pex) {
-            //this.send(message, user);
-        //}
+        }
 
         catch (IOException | URISyntaxException e) {
             e.printStackTrace();
@@ -73,5 +78,25 @@ public class AppointmentScraper {
 
     public void setHttpClient(HttpClient httpClient) {
         this.httpClient = httpClient;
+    }
+
+    private SSLSocketFactory socketFactory() {
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+        }};
+
+        try {
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            return sslContext.getSocketFactory();
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException("Failed to create a SSL socket factory", e);
+        }
     }
 }
